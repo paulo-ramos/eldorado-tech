8e668d2f6ad01fdc9ad57eba59aa10a29965fdcd
paulo-ramos <paulo_ramos@live.com>
2022-04-04 20:48:14 -0300

    add changes to migrations - code-first

---
+++ migrations/src/config/database.js
@@ -0,0 +1,9 @@
+module.exports = {
+    database:"autoservice", 
+    username: "root", 
+    password: "1q2w3e4r@#$",
+    dialect: "mysql",
+    host:"localhost",
+    port:"3306",
+    charset: "UTF8_GENERAL_CI"
+}
